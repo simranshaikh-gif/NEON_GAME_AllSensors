c8f6e17924c0d184ebea14fec1626500a69c44ae
simranshaikh-gif <simranbishaik@gmail.com>
2026-02-27 23:03:07 +0530

    Remove eclipse metadata and clean repo

---
+++ NEON_DASHBOARD/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
---
+++ NEON_DASHBOARD/.pio/build/esp32dev/libed7/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/libed7/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/sim_s/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
